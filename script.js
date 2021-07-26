76b0232e3973f19c1db3246a11ec3b9912aafc5c
André Rafael Rockenbach de Azambuja <andre.azambuja@kunden.com.br>
2021-07-26 11:31:57 -0300

    ajustes para usar apenas um <button>

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const elements = document.querySelectorAll('.switch');
-const btnDay = document.getElementById("btnDay")
-const btnNight = document.getElementById("btnNight")
+const elements = document.querySelectorAll(".switch");
 
-const action = () => {
+// Inicia a página com modo diurno por padrão.
+elements.forEach((element) => {
+  element.classList.add("day");
+});
 
-  elements.forEach( element =>{
-    element.classList.toggle('day')
-    element.classList.toggle('night')
-  })
- 
-  btnDay.classList.toggle('hide');
-  btnNight.classList.toggle('hide');
-
-}
+// Função para alterar o modo
+const changeMode = () => {
+  elements.forEach((element) => {
+    element.classList.toggle("day");
+    element.classList.toggle("night");
+  });
+};
