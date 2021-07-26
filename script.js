b3a01de4a92de3133dd635a4a1277c4c34e85c9b
André Rafael Rockenbach de Azambuja <andre.azambuja@kunden.com.br>
2021-07-26 13:30:59 -0300

    remoção ';' javascript

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const elements = document.querySelectorAll(".switch");
+const elements = document.querySelectorAll(".switch")
 
 // Configuração inicial da página.  "day" == modo diurno | "night" == modo noturno
 elements.forEach((element) => {
-  element.classList.add("day");
+  element.classList.add("day")
 });
 
 // Função para alterar o modo
 const changeMode = () => {
   elements.forEach((element) => {
-    element.classList.toggle("day");
-    element.classList.toggle("night");
+    element.classList.toggle("day")
+    element.classList.toggle("night")
   });
 };
